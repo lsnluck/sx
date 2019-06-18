6b83922c5f743508818e10ebf69115ddbaf31b83
lsnluck <1143596566@qq.com>
2019-06-18 15:20:57 +0800

    Initial commit

---
+++ todolist/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from todolist import views
+app_name='todolist'
+urlpatterns = [
+    path('home/', views.home,name='主页'),
+    path('edit/', views.edit,name='编辑'),
+    path('about/', views.about, name='关于'),
+
+]
---
+++ todolist/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+lst=[
+    {'待办事项':'吃饭','已完成':False},
+    {'待办事项': '逛街', '已完成': False},
+
+]
+def home(request):
+    global lst
+    print(request.method)
+    if request.method == "POST":
+        content = request.POST.get('待办事项')
+        print(content)
+        if not content or content.strip() == '':
+            return render(request, 'todolist/home.html', {'清单': lst,'警告':'请输入内容'})
+        else:
+            lst.append({'待办事项': content , '已完成': False})
+            return render(request, 'todolist/home.html', {'清单': lst})
+    else:
+        return render(request, 'todolist/home.html', {'清单': lst})
+
+def edit(request):
+    return render(request, 'todolist/edit.html')
+
+def about(request):
+    return render(request, 'todolist/about.html')
+
